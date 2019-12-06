48d46d5841c2d2661fe58694a268ea1ae620b480
Gabriel Jie <43084604+GabrielJie@users.noreply.github.com>
2019-12-06 10:46:20 +0800

    Add files via upload
    2D filtering function

---
+++ filtering2d.m
@@ -0,0 +1,22 @@
+function [H,Hs] = filtering2d(nelx, nely, nele, rmin)
+iH = ones(nele*(2*(ceil(rmin)-1)+1)^2,1);
+jH = ones(size(iH));
+sH = zeros(size(iH));
+k = 0;
+for i1 = 1:nelx
+    for j1 = 1:nely
+        e1 = (i1-1)*nely+j1;
+        for i2 = max(i1-(ceil(rmin)-1),1):min(i1+(ceil(rmin)-1),nelx)
+            for j2 = max(j1-(ceil(rmin)-1),1):min(j1+(ceil(rmin)-1),nely)
+                e2 = (i2-1)*nely+j2;
+                k = k+1;
+                iH(k) = e1;
+                jH(k) = e2;
+                sH(k) = max(0,rmin-sqrt((i1-i2)^2+(j1-j2)^2));
+            end
+        end
+    end
+end
+H = sparse(iH,jH,sH);
+Hs = sum(H,2);
+end
