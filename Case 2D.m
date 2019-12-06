14d2b6d262c91a938f073a17449de19673943cdb
Gabriel Jie <43084604+GabrielJie@users.noreply.github.com>
2019-12-06 10:49:33 +0800

    Add files via upload
    different cases in 2D

---
+++ Case 2D.m
@@ -0,0 +1,69 @@
+function Case
+%% load 1
+% Macro_struct = [10, 5, 100, 50, 0.4];
+% Micro_struct = [0.1, 0.1, 50, 50, 0.5];
+% Load_CASE = 1; penal = 3; rmin = 2; ft = 2;
+% ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.2];
+Micro_struct = [0.1, 0.1, 50, 50, 1];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.3];
+Micro_struct = [0.1, 0.1, 50, 50, 0.2/0.3];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.4];
+Micro_struct = [0.1, 0.1, 50, 50, 0.5];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.5];
+Micro_struct = [0.1, 0.1, 50, 50, 0.4];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.6];
+Micro_struct = [0.1, 0.1, 50, 50, 0.2/0.6];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.7];
+Micro_struct = [0.1, 0.1, 50, 50, 0.2/0.7];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.8];
+Micro_struct = [0.1, 0.1, 50, 50, 0.2/0.8];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.9];
+Micro_struct = [0.1, 0.1, 50, 50, 0.2/0.9];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 1];
+Micro_struct = [0.1, 0.1, 50, 50, 0.2];
+Load_CASE = 1; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+%% load 2
+% Macro_struct = [10, 5, 100, 50, 0.4];
+% Micro_struct = [0.1, 0.1, 50, 50, 0.5];
+% Load_CASE = 2; penal = 3; rmin = 2; ft = 2;
+% ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.4];
+Micro_struct = [0.1, 0.1, 50, 50, 0.5];
+Load_CASE = 2; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [10, 5, 100, 50, 0.5];
+Micro_struct = [0.1, 0.1, 50, 50, 0.4];
+Load_CASE = 2; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+%% load 3
+Macro_struct = [15, 3, 150, 30, 0.4];
+Micro_struct = [0.1, 0.1, 50, 50, 0.5];
+Load_CASE = 3; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+Macro_struct = [15, 3, 150, 30, 0.4];
+Micro_struct = [0.1, 0.1, 50, 50, 0.5];
+Load_CASE = 3; penal = 4; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
+% load 4
+Macro_struct = [10, 4, 100, 40, 0.4];
+Micro_struct = [0.1, 0.1, 50, 50, 0.5];
+Load_CASE = 4; penal = 3; rmin = 2; ft = 3;
+ConTop2D(Macro_struct, Micro_struct, Load_CASE, penal, rmin, ft)
